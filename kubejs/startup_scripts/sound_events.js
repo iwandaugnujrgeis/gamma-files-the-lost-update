33a769c966888c0480c62f4312e832426d7b8b36
zharok_01 <maxymmosolov@gmail.com>
2025-12-19 15:34:19 +0300

    Clean repository reset (4.2-indev)

---
+++ kubejs/startup_scripts/sound_events.js
@@ -0,0 +1,7 @@
+StartupEvents.registry('sound_event', (event) => {
+	event.create('gamma:music.skylands')
+	event.create('gamma:music.nether')
+	event.create('gamma:music_disc.dog')
+	event.create('gamma:music_disc.eleven')
+	event.create('gamma:music_disc.droopy2')
+})
